b8358a40e76f2f9f1471dd3735616c1bb5e8426a
Raj Kadir <rajkadir@hotmail.co.uk>
2016-02-14 15:23:13 +0000

    placed all logic code into cpp rather than in the header files

--- HierarchyTree/include/Node.h
+++ HierarchyTree/include/Node.h
@@ -1,41 +1,14 @@
 #ifndef NODE_H
 #define NODE_H
-#include "Member.h"
 
 class Node
 {
     public:
         Node();
 
-
-        Node* GetNewNode(int data){
-            // Create new node
-            Node* newNode = new Node();
-            // Set values of data and left/right child
-            newNode->data = data;
-            newNode->left = newNode->right = 0;
-        }
-
-        Node* Insert(Node* root, int data){
-            if(root == 0){ // Empty Tree
-                root = GetNewNode(data);
-
-            }
-            else if(data <= root->data){
-                root->left = Insert(root->left, data);
-            }
-            else{
-                root->right = Insert(root->right, data);
-            }
-            return root;
-        }
-
-        bool Search(Node* root, int data){
-            if(root == 0) return false;
-            else if(root->data == data) return true;
-            else if(data <= root->data) return Search(root->left, data);
-            else return Search(root->right, data);
-        }
+        Node* GetNewNode(int data);
+        Node* Insert(Node* root, int data);
+        bool Search(Node* root, int data);
 
         // Getters and setters
         int getData() { return data; }
