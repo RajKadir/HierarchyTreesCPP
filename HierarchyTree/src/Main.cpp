d9838cc97af1faf96a343d3a5fa402d70128a329
Raj Kadir <rajkadir@hotmail.co.uk>
2016-02-14 15:27:45 +0000

    placed bst struct into it's own file

--- HierarchyTree/src/Main.cpp
+++ HierarchyTree/src/Main.cpp
@@ -3,61 +3,22 @@
 #include <iostream>
 using namespace std;
 
-struct BstNode {
-    int data;
-    BstNode* left;
-    BstNode* right;
-};
-
-// Create a new node in dynamic memory
-BstNode* GetNewNode(int data){
-    // Create new node
-    BstNode* newNode = new BstNode();
-    // Set values of data and left/right child
-    newNode->data = data;
-    newNode->left = newNode->right = NULL;
-}
-
-BstNode* Insert(BstNode* root, int data){
-    if(root == NULL){ // Empty Tree
-        root = GetNewNode(data);
-
-    }
-    else if(data <= root->data){
-        root->left = Insert(root->left, data);
-    }
-    else{
-        root->right = Insert(root->right, data);
-    }
-    return root;
-}
-
-bool Search(BstNode* root, int data){
-    if(root == NULL) return false;
-    else if(root->data == data) return true;
-    else if(data <= root->data) return Search(root->left, data);
-    else return Search(root->right, data);
-}
-
 int main(){
 
-/*    BstNode* root = NULL; // Create an empty tree
-    root = Insert(root, 10);
-    root = Insert(root, 50);
-    root = Insert(root, 5);
-*/
-
+    // Create the tree
     Node* root = 0;
     root = root->Insert(root, 10);
     root = root->Insert(root, 50);
     root = root->Insert(root, 60);
 
-
-
+    // Hold input
     int number;
 
+    // Ask for input
     cout << "Enter a number to search in the tree \n";
     cin >> number;
+
+    // Check if this number is inside the tree
     if(root->Search(root, number) == true) cout << "Found\n";
     else cout << "Not found \n";
 }
